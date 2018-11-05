c551ccc0af992e34bc47c743ce6ffd99495e5ad6
lianghongh <lianghongh@126.com>
2018-11-05 16:20:58 +0800

    修改bug

--- main.cpp
+++ main.cpp
@@ -7,16 +7,20 @@ using namespace std;
 
 int main() {
 
-    string path="/home/lianghong/schedule/example.tgff";
-    string mac_path = "/Users/lianghong/Documents/sched_data/node_300/test.tgff";
+    char mac_path[1000];
+    int node_size=500;
+    sprintf(mac_path,"/Users/lianghong/Documents/paper_data/node_%d/test.tgff",node_size);
 
 
     TaskGraph g;
+
     g.build(mac_path);
-//    cout << g << "\n";
-    simple_ga(g,50, 2000, string("/Users/lianghong/Documents/sched_data/node_300/simple_ga"),0.2, 0.8);
-    cuckoo_search(g,50,2000,string("/Users/lianghong/Documents/sched_data/node_300/cuckoo"),0.25,0.03,0.04);
-    NSGA2(g,50,2000,string("/Users/lianghong/Documents/sched_data/node_300/MOPGA"),0.8,0.2);
+    sprintf(mac_path,"/Users/lianghong/Documents/paper_data/node_%d/simple_ga",node_size);
+    simple_ga(g,50, 5000, mac_path,0.2, 0.8);
+    sprintf(mac_path,"/Users/lianghong/Documents/paper_data/node_%d/cuckoo",node_size);
+    cuckoo_search(g,50,5000,mac_path,0.25,0.03,0.04);
+    sprintf(mac_path,"/Users/lianghong/Documents/paper_data/node_%d/MOPGA",node_size);
+    NSGA2(g,50,5000,mac_path,0.8,0.2);
 
     return 0;
 }
